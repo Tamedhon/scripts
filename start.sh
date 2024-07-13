9fbd5b1b027b9c362a41542c35e9515e96b2156f
Aloy <44802344+V-Cyberpunk@users.noreply.github.com>
2024-07-13 18:53:33 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 MUD_ROOT=/home/mud
+TELNET_PORT=4711
 
+TLS_PORT=4712
 TLS_KEY="$MUD_ROOT/tls/tamedhon.key"
 TLS_CERT="$MUD_ROOT/tls/tamedhon.crt"
 TLS_ISSUER="$MUD_ROOT/tls/tamedhon.issuer.crt"
@@ -9,4 +11,6 @@ TLS_ISSUER="$MUD_ROOT/tls/tamedhon.issuer.crt"
 $MUD_ROOT/bin/ldmud \
   --tls-key="${TLS_KEY}" \
   --tls-cert="${TLS_CERT}" \
-  --tls-trustfile="${TLS_ISSUER}"
+  --tls-trustfile="${TLS_ISSUER}" \
+  $TELNET_PORT \
+  $TLS_PORT
