5ddf9a8b66733ef6471befba42182d8f6a2429da
Aloy <44802344+V-Cyberpunk@users.noreply.github.com>
2024-11-16 02:16:18 +0100

    Delete tinyfugue directory

--- tinyfugue/muenze.tf
+++
@@ -1,5 +0,0 @@
- /def muenzen = nimm 131072 muenzen%;nimm 65536 muenzen%;nimm 32768 muenzen%;\
- nimm 16384 muenzen %;nimm 8192 muenzen%;nimm 4096 muenzen%;nimm 2048 muenzen%;\
- nimm 1024 muenzen%;nimm 512 muenzen%;  nimm 256 muenzen%;nimm 128 muenzen%;\
- nimm 64 muenzen%;nimm 32 muenzen%;nimm 16 muenzen%;nimm 8 muenzen%;\
- nimm 4 muenzen%;nimm 2 muenzen%;nimm muenze%;
